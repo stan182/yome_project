61c252d797e39373056984fa86a250c0915fc1fe
stan182 <stanislav.emshanov@gmail.com>
2019-03-03 14:27:40 +0400

    map event on home

--- app/src/screens/Home.tsx
+++ app/src/screens/Home.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { View, ScrollView, Text, TouchableOpacity } from "react-native";
 import { NavigationInjectedProps, NavigationScreenProps } from "react-navigation";
 import { homeScreenStyles as styles } from "../general/styles";
+import _ from "lodash";
 
 interface HomeScreenProps {
     navigation: NavigationScreenProps<any, any>;
@@ -26,36 +27,13 @@ export default class Home extends Component<HomeProps> {
                 <View style={styles.mainContainer}>
                     <ScrollView>
                          <View style={styles.container}>
+                            {_.map(events, event => (
                              <TouchableOpacity onPress={this.handler}>
                                 <View style={styles.eventContainer} >
-                                    <Text style={{fontSize: 24}}>EVENT</Text>
+                                    <Text style={{fontSize: 24}}>{event.comment}</Text>
                                 </View>
                              </TouchableOpacity>
-                             <TouchableOpacity onPress={this.handler}>
-                                <View style={styles.eventContainer}>
-                                    <Text style={{fontSize: 24}}>EVENT</Text>
-                                </View>
-                             </TouchableOpacity>
-                             <TouchableOpacity onPress={this.handler}>
-                                <View style={styles.eventContainer}>
-                                    <Text style={{fontSize: 24}}>EVENT</Text>
-                                </View>
-                             </TouchableOpacity>
-                             <TouchableOpacity onPress={this.handler}>
-                                <View style={styles.eventContainer}>
-                                    <Text style={{fontSize: 24}}>EVENT</Text>
-                                </View>
-                             </TouchableOpacity>
-                             <TouchableOpacity onPress={this.handler}>
-                                <View style={styles.eventContainer}>
-                                    <Text style={{fontSize: 24}}>EVENT</Text>
-                                </View>
-                             </TouchableOpacity>
-                             <TouchableOpacity onPress={this.handler}>
-                                <View style={styles.eventContainer}>
-                                    <Text style={{fontSize: 24}}>EVENT</Text>
-                                </View>
-                             </TouchableOpacity>
+                            )}
                          </View>
                     </ScrollView>
                 </View>
